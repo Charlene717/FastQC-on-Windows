850d12611072bb488da924b928471db680a5de7d
Charlene_MSI_AI <q2330416@gmail.com>
2025-02-16 17:24:14 +0800

    Ori

---
+++ Sum_FastQC.R
@@ -0,0 +1,49 @@
+# 載入必要的套件
+library(tidyverse)
+library(zip)
+
+# 設定 FastQC 結果所在的資料夾
+fastqc_dir <- "你的資料夾路徑"
+
+# 取得所有 .zip 檔案的路徑
+fastqc_files <- list.files(fastqc_dir, pattern = "_fastqc.zip$", full.names = TRUE)
+
+# 解析 summary.txt 的函數
+parse_fastqc_summary <- function(zip_file) {
+  # 解壓縮並讀取 summary.txt
+  temp_dir <- tempdir()
+  unzip(zip_file, files = "*/summary.txt", exdir = temp_dir)
+  
+  # 找到 summary.txt 的實際路徑
+  summary_file <- list.files(temp_dir, pattern = "summary.txt$", full.names = TRUE, recursive = TRUE)
+  
+  if (length(summary_file) == 0) {
+    return(NULL)
+  }
+  
+  # 讀取 summary.txt
+  summary_data <- read.delim(summary_file, header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+  colnames(summary_data) <- c("Status", "Metric", "Filename")
+  
+  # 取得樣本名稱
+  sample_name <- gsub("_fastqc.zip", "", basename(zip_file))
+  summary_data$Sample <- sample_name
+  
+  return(summary_data)
+}
+
+# 讀取所有 FastQC 的 QC 結果
+fastqc_results <- lapply(fastqc_files, parse_fastqc_summary)
+
+# 合併成一個 dataframe
+fastqc_summary_df <- bind_rows(fastqc_results)
+
+# 重新排列欄位順序
+fastqc_summary_df <- fastqc_summary_df %>%
+  select(Sample, Metric, Status)
+
+# 將整理後的結果輸出
+write.csv(fastqc_summary_df, file = "FastQC_summary.csv", row.names = FALSE)
+
+# 顯示前幾行
+print(head(fastqc_summary_df))
